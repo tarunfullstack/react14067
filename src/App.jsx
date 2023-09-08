01b4bf6f5d332470596fe8592973cb7269c2e3ee
tarunfullstack <dtarunkumar123@gmail.com>
2023-09-08 21:25:17 +0530

    ssd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { useFormik } from "formik";
+import *as Yup from 'yup'
 function App(){
+   var myform = useFormik({
+        initialValues : {
+            firstname : '',
+            email : '',
+        },
+        validationSchema : Yup.object({
+            firstname : Yup.string().required("asdfg"),
+            email : Yup.string().email().required("qwert")
+        }),
+        onSubmit : (x)=>{
+        console.log(x)
+        }
+    })
     return(
-        <div className="mybox">
-            <h1>My App</h1>
-            <Link to="/for">Form</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to="/asd">ScoreCard</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to="/zxc">Vacation</Link>
-            <Outlet></Outlet>
-            
+        <div style={{border:'2px solid blue',padding:'10px',margin:'10px'}}>
+            <h1>Formik Practical Class</h1>
+            <div style={{border:'2px solid green',padding:'10px',margin:'10px'}}></div>
+            <form onSubmit={myform.handleSubmit}>
+                <input type="text" name="firstname" onChange={myform.handleChange}/>
+                <br />
+                <input type="email" name="email" onChange={myform.handleChange}/>
+                <br />
+                {JSON.stringify(myform.errors)}
+                <button>Save</button>
+            </form>
         </div>
     )
 }
-export default App
+export default App;
